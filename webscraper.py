cb2327eef85be5ddbf1d920e4eb519987ac61bbe
Andre Paul <andrepaul8128@gmail.com>
2020-08-26 15:27:29 -0700

    beautifulsoup changes, WIP

--- webscraper.py
+++ webscraper.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template
 from bs4 import BeautifulSoup
 import requests
 
-redraft_txt = requests('https://www.fantasypros.com/nfl/rankings/consensus-cheatsheets.php').text
-dynasty_txt = requests('https://www.fantasypros.com/nfl/rankings/dynasty-overall.php').text
+redraft_url = 'https://www.fantasypros.com/nfl/rankings/consensus-cheatsheets.php'
+dynasty_url = 'https://www.fantasypros.com/nfl/rankings/dynasty-overall.php'
+
+redraft_txt = requests.get(redraft_url).text
+dynasty_txt = requests.get(dynasty_url).text
+
+redraft_soup = BeautifulSoup.(redraft_txt, 'html.parser')
+dynasty_soup = BeautifulSoup.(dynasty_txt, 'html.parser')
+
 
