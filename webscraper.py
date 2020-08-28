62bc01e4bf389783378454aa50a6e9ad360bac88
Andre Paul <andrepaul8128@gmail.com>
2020-08-28 11:44:56 -0700

    webscraper is working!

--- webscraper.py
+++ webscraper.py
@@ -1,14 +1,31 @@
 from flask import Flask, render_template
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 import requests
 
-redraft_url = 'https://www.fantasypros.com/nfl/rankings/consensus-cheatsheets.php'
-dynasty_url = 'https://www.fantasypros.com/nfl/rankings/dynasty-overall.php'
+rd_url = 'https://www.fantasypros.com/nfl/rankings/consensus-cheatsheets.php'
+dy_url = 'https://www.fantasypros.com/nfl/rankings/dynasty-overall.php'
 
-redraft_txt = requests.get(redraft_url).text
-dynasty_txt = requests.get(dynasty_url).text
+rd_resp = requests.get(rd_url)
+dy_resp = requests.get(dy_url)
 
-redraft_soup = BeautifulSoup.(redraft_txt, 'html.parser')
-dynasty_soup = BeautifulSoup.(dynasty_txt, 'html.parser')
+rd_soup = bs(rd_resp.text, 'html.parser')
+dy_soup = bs(dy_resp.text, 'html.parser')
 
+rd_ranks = []
+dy_ranks = []
 
+for tr in rd_soup.find_all('tr'):
+    td = tr.find_all(class_="full-name")
+    if(td != []):
+        rd_ranks.append(td[0].text)
+
+for tr in dy_soup.find_all('tr'):
+    td = tr.find_all(class_="full-name")
+    if(td != []):
+        dy_ranks.append(td[0].text)
+
+for elt in rd_ranks[:10]:
+    print(elt)
+
+for elt in dy_ranks[:10]:
+    print(elt)
